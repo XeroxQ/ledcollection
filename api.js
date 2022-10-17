0c4747870d0f31641e08372105d562e90669d3c0
Martijn Poppen <m.poppen@ziggo.nl>
2022-10-17 13:46:32 +0200

    NEW: add support to set screensaver from external apps (Homesh)

--- api.js
+++ api.js
@@ -1,5 +1,18 @@
 module.exports = {
     async settings({ homey, body }){
         return homey.app.updateSettings( body );
+    },
+    async getScreensavers({ homey }){
+        return Object.keys(homey.app.animations);
+    },
+    async setScreensaver({ homey, params }){
+        const animation = homey.app.animations[`${params.id}`];
+        if(animation) {
+            homey.app.log('Setting animation', params.id);
+            animation.start();
+        }
     }
+    // this.ledRingApi = this.homey.api.getApiApp('openmindnl.ledcollection');
+    // const getScreensavers = await this.ledRingApi.get('/screensavers');
+    // const setScreensaver = await this.ledRingApi.post(`/screensaver/${sceensaverID}`);
 };
